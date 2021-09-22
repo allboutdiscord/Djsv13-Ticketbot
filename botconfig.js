53e47e0a6842a058fded29f1cfe8b9aba0743f98
allboutdiscord <86099849+allboutdiscord@users.noreply.github.com>
2021-09-22 12:22:42 +0530

    Add files via upload

---
+++ botconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: "!",
+  status: {
+    		name: "Tickets!",
+    		type: "watching"
+  },
+   emojis: {
+        giveaway: "🎉",
+        special: "🔴",
+        general: "✅"
+  },
+  ticketembed: {
+    title: "Tickets",
+    description: "To create a ticket, click the button that suits your request!",
+    footer: "By AllBoutDiscord"
+  },
+};
